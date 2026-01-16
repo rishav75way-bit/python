9ca863f8e4c8b731f9ca5aae3cc06623eec2486e
rishav75way-bit <rishav.75way@gmail.com>
2026-01-16 18:20:16 +0530

    python

--- src/app/main.py
+++ src/app/main.py
@@ -3,9 +3,9 @@ from sqlmodel import SQLModel
 from src.app.db.session import engine
 import src.app.models.user
 
-from src.app.routes.health import router as health_router
-from src.app.routes.auth import router as auth_router
-from src.app.routes.users import router as users_router
+from src.app.api.routes.health import router as health_router
+from src.app.api.routes.auth import router as auth_router
+from src.app.api.routes.users import router as users_router
 
 app = FastAPI(title="python-practice")
 
